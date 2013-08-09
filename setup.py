fd6a139c71e253335b1eadc77446fd5945b04a6d
Iuri de Silvio <iurisilvio@gmail.com>
2013-08-09 11:00:25 -0300

    Add some metadata to PyPI in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,22 @@ from setuptools import setup
 PACKAGE = 'freedomsponsors-trac-plugin'
 VERSION = '0.1'
 
+try:
+    long_description = open("README.md").read()
+except:
+    long_description = ''
+
 setup(name=PACKAGE,
       version=VERSION,
       packages=['freedomsponsors'],
       entry_points={'trac.plugins': '%s = freedomsponsors' % PACKAGE},
+      description="FreedomSponsors Trac plugin",
+      long_description=long_description,
+      author='Iuri de Silvio',
+      author_email='iurisilvio@gmail.com',
+      url='http://github.com/iurisilvio/%s' % PACKAGE,
+      keywords='trac plugin',
+      license='MIT',
+      zip_safe=False,
 )
+
